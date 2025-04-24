255149252fb10ddc1cab23636699fc8ebbd119a4
Webdrian <tools@webdrian.de>
2025-04-24 23:15:15 +0200

    Update __init__.py

---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+from .fetching import fetch_html_and_requests, extract_meta
+from .cms import detect_cms, detect_wordpress_theme, detect_plugins, detect_technologies
+from .dsgvo import evaluate_risks
+from .cookies import analyze_cookies
+from .legal_check import check_legal_pages
